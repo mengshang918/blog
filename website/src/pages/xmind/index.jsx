f511099aee00a021bb04a4a8ea5176cc4bd04d40
mengshang918 <1446269124@qq.com>
2020-09-16 23:48:51 +0800

    feat: 添加思维导图页

---
+++ website/src/pages/xmind/index.jsx
@@ -0,0 +1,43 @@
+import React,{useEffect,useCallback,useState} from 'react'
+import Layout from '@theme/Layout'
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import useBaseUrl from '@docusaurus/useBaseUrl'
+
+function Xmind() {
+  const [height,setHeight] = useState(0)
+  const context = useDocusaurusContext()
+  const { siteConfig = {} } = context
+
+useEffect(()=>{
+    setHeight(
+      document.querySelector('.main-wrapper').getBoundingClientRect().height
+    )
+
+},[])
+
+  const getWidth = useCallback(()=>{
+    setHeight(document.querySelector('.main-wrapper').getBoundingClientRect().height)
+  })
+
+  useEffect(()=>{
+    window.addEventListener('resize', getWidth)
+    return ()=>{
+window.removeEventListener('resize',getWidth)
+    }
+  })
+
+  return (
+    <Layout title={`${siteConfig.title}|思维导图`}>
+      <iframe
+        src={useBaseUrl('xmind.html')}
+        frameborder="0"
+        style={{minHeight:600}}
+        height={height}
+        width="100%"
+
+      ></iframe>
+    </Layout>
+  )
+}
+
+export default Xmind
