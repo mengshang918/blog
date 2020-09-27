880ba77de07beefdd58eaba2530014ba0dbdd8fb
mengshang918 <1446269124@qq.com>
2020-09-27 23:12:30 +0800

    chore: 优化todoPage生成脚本

--- scripts/createTodoPage.js
+++ scripts/createTodoPage.js
@@ -6,7 +6,7 @@
  * @Author: jiangxiaowei
  * @Date: 2020-09-18 14:57:52
  * @Last Modified by: jiangxiaowei
- * @Last Modified time: 2020-09-18 18:34:11
+ * @Last Modified time: 2020-09-27 23:10:52
  */
 
 const fs = require('fs')
@@ -17,26 +17,35 @@ const ignoreDir = ['.DS_Store', 'xmind.html', 'xmind.png']
 const data = []
 const loadDir = (entryPath) => {
   const files = fs.readdirSync(entryPath)
-
-  files.map((item) => {
-    if (ignoreDir.includes(item)) {
-      return
-    }
+  // 判断是否需要加入todo页面
+  const needPushToDo = files.some((item) => {
     const filePath = path.resolve(entryPath, item)
     const stat = fs.statSync(filePath)
-    const bararr = filePath
-      .split('/docs/')[1]
-      .split('/')
-      .filter((item) => item)
-    const barL = bararr.length + 1
-    if (stat.isDirectory()) {
-      data.push(`${'#'.repeat(barL)} ${bararr[bararr.length - 1]}`)
-      loadDir(filePath)
-    } else if (path.basename(filePath) === 'TODO.md') {
-      const { content } = matter.read(filePath)
-      data.push(`${content.replace(/\n/g, '\n> ')}\n`)
-    }
+    return stat.isDirectory() || item === 'TODO.md'
   })
+  if (needPushToDo) {
+    files.map((item) => {
+      if (ignoreDir.includes(item)) {
+        return
+      }
+      const filePath = path.resolve(entryPath, item)
+      const stat = fs.statSync(filePath)
+      const bararr = filePath
+        .split('/docs/')[1]
+        .split('/')
+        .filter((item) => item)
+      const barL = bararr.length + 1
+      if (stat.isDirectory()) {
+        data.push(`${'#'.repeat(barL)} ${bararr[bararr.length - 1]}`)
+        loadDir(filePath)
+      } else if (path.basename(filePath) === 'TODO.md') {
+        const { content } = matter.read(filePath)
+        data.push(`${content.replace(/\n/g, '\n> ')}\n`)
+      }
+    })
+  } else {
+    data.pop()
+  }
 }
 
 loadDir(path.resolve(__dirname, '../docs/'))
